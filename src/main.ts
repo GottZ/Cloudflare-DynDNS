68df5436be4a48990525027eaa037379e6228326
GottZ <git@gottz.de>
2024-04-08 23:32:48 +0200

    initial route layout plan

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,20 @@ export const router = OpenAPIRouter({
 
 router.get("/api/users/", UserList);
 router.get("/api/users/:userName/", UserFetch);
+//router.put("/api/users/:userName/", UserCreate);
+//router.delete("/api/users/:userName/", UserDelete);
+
+//router.get("/api/dns/:userName/", DnsFetch);
+//router.put("/api/dns/:userName/", DnsCreate);
+//router.delete("/api/dns/:userName/", DnsDelete);
+
+//router.get("/api/logs/:userName/", LogsFetch);
+//router.get("/api/logs/", LogsList);
+
+//router.get("/nic/update", NicUpdate);
+//router.get("/update", NicUpdate);
+
+//router.get("/robots.txt", RobotsTxt);
 
 router.all("*", () => Response.json({
         success: false,
